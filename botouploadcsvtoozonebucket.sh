3241388f8e6a6d2f29801b3138a80ce370856e9e
Marc Chisinevski <marcgcp6@gmail.com>
2023-05-09 23:03:30 -0700

    Update botouploadcsvtoozonebucket.sh

--- botouploadcsvtoozonebucket.sh
+++ botouploadcsvtoozonebucket.sh
@@ -1,3 +1,4 @@
+
 #sudo python -m pip install boto3
 import boto3
 from botocore.client import Config
@@ -6,3 +7,33 @@ s3= boto3.resource('s3',endpoint_url='https://ip-10-10-72-69.us-west-2.compute.i
 s3.create_bucket(Bucket='spark-bucket')
 
 s3.Bucket('spark-bucket').upload_file('/home/ec2-user/imdbkaggle.csv','kaggledataset/imdbkaggle.csv')
+
+###CHECK
+#[root@ip-10-10-72-69 ec2-user]# ozone sh key info /s3v/spark-bucket/kaggledataset/imdbkaggle.csv
+#{
+#  "volumeName" : "s3v",
+#  "bucketName" : "spark-bucket",
+#  "name" : "kaggledataset/imdbkaggle.csv",
+#  "dataSize" : 11421938,
+#  "creationTime" : "2023-05-10T06:01:48.404Z",
+#  "modificationTime" : "2023-05-10T06:01:48.404Z",
+#  "replicationConfig" : {
+#    "replicationFactor" : "THREE",
+#    "requiredNodes" : 3,
+#    "replicationType" : "RATIS"
+#  },
+#  "ozoneKeyLocations" : [ {
+#    "containerID" : 2003,
+#    "localID" : 111677748019208560,
+#    "length" : 8388608,
+#    "offset" : 0,
+#    "keyOffset" : 0
+#  }, {
+#    "containerID" : 2004,
+#    "localID" : 111677748019208559,
+#    "length" : 3033330,
+#    "offset" : 0,
+#    "keyOffset" : 8388608
+#  } ],
+#  "metadata" : { }
+#}
