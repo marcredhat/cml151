e355ea9ac14bfa812770dc9aeac5aedb2e2cc4ba
Marc Chisinevski <marcgcp6@gmail.com>
2023-05-10 18:00:35 -0700

    Update text2image.py

--- text2image.py
+++ text2image.py
@@ -1,8 +1,8 @@
-
 #https://towardsdatascience.com/stable-diffusion-using-hugging-face-501d8dbdd8
 
 from diffusers import StableDiffusionPipeline
+pipe = StableDiffusionPipeline.from_pretrained('CompVis/stable-diffusion-v1-4').to('cuda')
 # Initialize a prompt
-prompt = "a dog wearing hat"
+prompt = "a monkey wearing hat"
 # Pass the prompt in the pipeline
 pipe(prompt).images[0]
